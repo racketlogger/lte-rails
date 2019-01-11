9d22312a105bfd25362ae3d30c8ecc09c0bdfdf4
Carlos Puchol <cpg+git@amahi.org>
2019-01-11 11:53:51 -0800

    update version for release

--- lib/lte/rails/version.rb
+++ lib/lte/rails/version.rb
@@ -1,7 +1,7 @@
 module Lte
   module Rails
     # NOTE: this needs to be changed in the Makefile at the same time!
-    VERSION = "0.0.5"
+    VERSION = "0.1.0"
     ADMIN_LTE_VERSION = "2.3.5"
   end
 end
